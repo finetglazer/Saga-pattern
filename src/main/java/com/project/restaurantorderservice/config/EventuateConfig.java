6dec309e38aacd80d15ae7c2562b79d92e89f535
I hate iu <tranhung10122003@gmail.com>
2025-03-16 21:11:44 +0700

    feat: update

--- src/main/java/com/project/restaurantorderservice/config/EventuateConfig.java
+++ src/main/java/com/project/restaurantorderservice/config/EventuateConfig.java
@@ -1,25 +1,13 @@
 // File: src/main/java/com/project/restaurantorderservice/config/EventuateConfig.java
 package com.project.restaurantorderservice.config;
 
-
-import io.eventuate.tram.sagas.spring.orchestration.SagaOrchestratorConfiguration;
-import io.eventuate.tram.sagas.spring.participant.SagaParticipantConfiguration;
-import io.eventuate.tram.spring.commands.producer.TramCommandProducerConfiguration;
-import io.eventuate.tram.spring.events.publisher.TramEventsPublisherConfiguration;
-import io.eventuate.tram.spring.messaging.producer.jdbc.TramMessageProducerJdbcConfiguration;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
 
+/**
+ * Simplified placeholder for EventuateConfig
+ * We're not actually importing any Eventuate configurations
+ */
 @Configuration
-@Import({
-        TramEventsPublisherConfiguration.class,
-        TramMessageProducerJdbcConfiguration.class,
-        TramCommandProducerConfiguration.class,
-        // Using our custom placeholder instead of TramJdbcKafkaConfiguration
-        TramJdbcKafkaConfiguration.class,
-        SagaOrchestratorConfiguration.class,
-        SagaParticipantConfiguration.class
-})
 public class EventuateConfig {
-    // Configuration class using imports from Eventuate
+    // This is just a placeholder now
 }
--- src/main/java/com/project/restaurantorderservice/config/OrderServiceConfiguration.java
+++ src/main/java/com/project/restaurantorderservice/config/OrderServiceConfiguration.java
@@ -3,13 +3,11 @@ package com.project.restaurantorderservice.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
 @EnableJpaRepositories(basePackages = "com.project.restaurantorderservice.domain")
+@EnableTransactionManagement
 public class OrderServiceConfiguration {
-
-    // Remove all @Bean methods for beans that are already annotated with
-    // @Service or @Component in their respective classes
-
-    // You can keep other configuration here that doesn't create bean definition conflicts
+    // Basic configuration only
 }
